57b246d1a56fd97c1c6e29fe4e05aaf505cc22d5
keita813 <k1t37.smile@gmail.com>
2020-02-08 10:26:56 +0000

    p

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   
-  root to: 'end_users/homes#top'
   get 'homes/about'
   get 'admins/top'
-  devise_for :end_users
+  devise_for :end_users,  :controllers => {
+    :sessions => 'end_users/sessions',
+    :registrations => 'end_users/registrations'
+    }
+  devise_for :admins,  :controllers => {
+    :sessions => 'admins/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admins do
   	#adminsのルートを下記に書く
@@ -12,5 +17,6 @@ Rails.application.routes.draw do
 
   namespace :end_users do
   	#end_usersのルートを下記に書く
+    root to: 'end_users/homes#top'
   end
 end
