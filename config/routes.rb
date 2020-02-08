e0c824f82bc0942a13ae965d8f3f8479e524a3da
keita813 <k1t37.smile@gmail.com>
2020-02-08 07:12:40 +0000

    pull

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get  'homes/top'
+  root to:  'homes#top'
+  get 'homes/about'
   get 'admins/top'
   devise_for :end_users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
