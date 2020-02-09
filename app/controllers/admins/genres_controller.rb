5b40fc2dac1f80dd930bdfe059d744466ad0b9c5
okamototoshitsugu <warld14tsugu@gmail.com>
2020-02-09 12:50:39 +0000

    push

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -1,2 +1,22 @@
 class Admins::GenresController < ApplicationController
+	def index
+		@genre = Genre.new
+		@genres = Genre.all
+	end
+
+	def create
+		@genre = Genre.new(genre_params)
+		@genre.save
+		redirect_to admins_genres_path
+	end
+
+	def edit
+	end
+
+	def update
+	end
+	private
+  def genre_params
+    params.require(:genre).permit(:name, :is_active )
+  end
 end
