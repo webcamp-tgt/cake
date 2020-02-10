ecf8d7b5e00e94b4e3815c0695c785c8a82aa022
okamototoshitsugu <warld14tsugu@gmail.com>
2020-02-10 12:57:32 +0000

    push

--- app/controllers/end_users/cart_items_controller.rb
+++ app/controllers/end_users/cart_items_controller.rb
@@ -1,2 +1,33 @@
 class EndUsers::CartItemsController < ApplicationController
+	def index
+		@cart_items = current_end_user.cart_items
+	end
+
+	def create
+		if @cart_item.blank?
+			@cart_item = current_caet.cart_item.build(item_id: params[:item_id])
+		end
+
+		@cart_item.quantity += params[:quantity].to_i
+		@cart_item.save
+		redirect_to current_cart
+	end
+
+	  # カート詳細画面から、「更新」を押した時のアクション
+	  def update
+	  	@cart_item.update(quantity: params[:quantity].to_i)
+	  	redirect_to current_cart
+	  end
+
+	　# カート詳細画面から、「削除」を押した時のアクション
+	def destroy
+		@cart_item.destroy
+		redirect_to current_cart
+	end
+
+	private
+
+	def setup_cart_item!
+		@cart_item = current_cart.cart_items.find_by(item_id: params[:item_id])
+	end
 end
