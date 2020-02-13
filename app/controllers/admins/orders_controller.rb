59b500defadcdc5aaaa5963469745f3e3c29e315
keita813 <k1t37.smile@gmail.com>
2020-02-13 08:35:15 +0000

    tabo

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -7,5 +7,12 @@ class Admins::OrdersController < ApplicationController
   	@order = order.find(params[:id])
   	
   end
+  def update
+  	@order =  Order.find(params[:id])
+  	@order.update(order_params)
+  	@order_item = Order_item.find(params[:id])
+  	@order_item.update(order_item_params)
+  	redirect_to admins_orders_path(@order)
+  end
 end
 
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,10 @@
 class OrderItem < ApplicationRecord
 	belongs_to :order
 	belongs_to :item
+
+	def total_price
+		price_tax * order_quantity
+	end
 end
+
+
