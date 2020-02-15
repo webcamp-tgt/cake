c6f39f20bdabb9fc330b0abeff199db32e6576ac
okamototoshitsugu <warld14tsugu@gmail.com>
2020-02-15 05:24:33 +0000

    push

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_12_050239) do
+ActiveRecord::Schema.define(version: 2020_02_15_022312) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 2020_02_12_050239) do
     t.string "phone_number"
     t.boolean "is_deleted"
     t.string "full_name"
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_end_users_on_deleted_at"
     t.index ["email"], name: "index_end_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
   end
@@ -84,7 +86,7 @@ ActiveRecord::Schema.define(version: 2020_02_12_050239) do
   create_table "orders", force: :cascade do |t|
     t.integer "end_user_id"
     t.integer "shipping_cost"
-    t.integer "payment_method", null: false
+    t.integer "payment_method"
     t.string "zip_code"
     t.string "address"
     t.string "order_name"
