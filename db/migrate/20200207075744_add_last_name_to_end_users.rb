1cd090ec95d928596b61f10df1ecd3bd06a27e04
okamototoshitsugu <warld14tsugu@gmail.com>
2020-02-19 04:16:43 +0000

    0

--- db/migrate/20200207075744_add_last_name_to_end_users.rb
+++ db/migrate/20200207075744_add_last_name_to_end_users.rb
@@ -1,12 +1,5 @@
 class AddLastNameToEndUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :end_users, :last_name, :string
-    add_column :end_users, :last_name_kana, :string
-    add_column :end_users, :first_name, :string
-    add_column :end_users, :first_name_kana, :string
-    add_column :end_users, :zip_code, :string
-    add_column :end_users, :address, :string
-    add_column :end_users, :phone_number, :string
     add_column :end_users, :is_deleted, :boolean
   end
 end
